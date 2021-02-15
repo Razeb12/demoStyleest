990368b0657d1e735a652876b4f0f6d95352e304
Razeb Enyi <enyirazeb@gmail.com>
2021-02-15 20:53:29 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,75 +1,76 @@
 <template>
-    <div>
-            
-  <!-- Footer -->
-  <footer id="footer" class="section footer">
-    <div class="container">
-      <div class="footer-container">
-        <div class="footer-center">
-          <h3>EXTRAS</h3>
-          <a href="#">Brands</a>
-          <a href="#">Gift Certificates</a>
-          <a href="#">Affiliate</a>
-          <a href="#">Specials</a>
-          <a href="#">Site Map</a>
-        </div>
-        <div class="footer-center">
-          <h3>INFORMATION</h3>
-          <a href="#">About Us</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="#">Contact Us</a>
-          <a href="#">Site Map</a>
-        </div>
-        <div class="footer-center">
-          <h3>MY ACCOUNT</h3>
-          <a href="#">My Account</a>
-          <a href="#">Order History</a>
-          <a href="#">Wish List</a>
-          <a href="#">blogletter</a>
-          <a href="#">Returns</a>
-        </div>
-        <div class="footer-center">
-          <h3>CONTACT US</h3>
-          <div>
-            <span>
-              <i class="fas fa-map-marker-alt"></i>
-            </span>
-            42 Dream House, Dreammy street, 7131 Dreamville, USA
+  <div>
+    <!-- Footer -->
+    <footer id="footer" class="section footer">
+      <div class="container">
+        <div class="footer-container">
+          <div class="footer-center">
+            <h3>EXTRAS</h3>
+            <a href="#">Brands</a>
+            <a href="#">Gift Certificates</a>
+            <a href="#">Affiliate</a>
+            <a href="#">Specials</a>
           </div>
-          <div>
-            <span>
-              <i class="far fa-envelope"></i>
-            </span>
-            company@gmail.com
+          <div class="footer-center">
+            <h3>INFORMATION</h3>
+            <a href="#">About Us</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Contact Us</a>
           </div>
-          <div>
-            <span>
-              <i class="far fa-paper-plane"></i>
-            </span>
-            Dream City, USA
+          <div class="footer-center">
+            <h3>MY ACCOUNT</h3>
+            <a href="#">My Account</a>
+            <a href="#">Order History</a>
+            <a href="#">Wish List</a>
+            <a href="#">Returns</a>
           </div>
+          <div class="footer-center">
+            <h3>CONTACT US</h3>
+            <div>
+              <span>
+                <i class="fas fa-map-marker-alt"></i>
+              </span>
+              160 Kemp House, City Roa, London
+            </div>
+            <div>
+              <span>
+                <i class="far fa-envelope"></i>
+              </span>
+              info@styleest.fashion
+            </div>
+            <div>
+              <span>
+                <i class="far fa-paper-plane"></i>
+              </span>
+              London. Uk
+            </div>
+          </div>
+          
         </div>
+        <hr class="line">
+        <div class="footer-center">
+                 <img src="../assets/whitelogo.png" alt="" class="img-fluid" />
+          </div>
       </div>
-    </div>
-  </footer>
-  <!-- End Footer -->
-    </div>
+    </footer>
+    <!-- End Footer -->
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Footer'
-}
+  name: "Footer",
+};
 </script>
 
 <style scoped>
-
 /* ========= Footer ======== */
 .footer {
   background-color: var(--black);
   padding: 6rem 1rem;
   line-height: 3rem;
+  width: 100vw;
 }
 
 .footer-center span {
@@ -89,7 +90,10 @@ export default {
   font-size: 1.4rem;
   transition: 0.6s;
 }
-
+.line {
+  background: white;
+  width: 100%;
+}
 .footer-center a:hover {
   color: var(--white);
 }
